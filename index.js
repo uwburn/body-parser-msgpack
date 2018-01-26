9031cb44705d994bb23a20bbf8df36e43f614a86
Michele Tibaldi <michele.tibaldi@holonix.it>
2018-01-26 16:13:32 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const read = require("body-parser/lib/read");
+const msgpack = require("msgpack5")();
+
+function typeChecker(type) {
+    return function checkType(req) {
+        return Boolean(typeis(req, type))
+    }
+}
+
+module.exports = function(bodyParser) {
+    if(bodyParser.msgpack)
+        return;
+
+    function extension(options) {
+        var opts = options || {};
+
+        var inflate = opts.inflate !== false;
+        var limit = typeof opts.limit !== 'number'
+            ? bytes.parse(opts.limit || '100kb')
+            : opts.limit;
+        var type = opts.type || 'application/msgpack';
+        var verify = opts.verify || false;
+
+        if (verify !== false && typeof verify !== 'function') {
+            throw new TypeError('option verify must be function')
+        }
+
+        // create the appropriate type checking function
+        var shouldParse = typeof type !== 'function'
+            ? typeChecker(type)
+            : type;
+
+        function parse(buf) {
+            if (buf.length === 0) {
+                debug('buffer is zero');
+                return {}
+            }
+
+            debug('parsing message-pack content');
+            return msgpack.decode(buf);
+        }
+
+        return function middleware(req, res, next) {
+            if (req._body) {
+                debug('body already parsed');
+                return next();
+            }
+
+            req.body = req.body || {};
+
+            // skip requests without bodies
+            if (!typeis.hasBody(req)) {
+                debug('skip empty body');
+                return  next();
+            }
+
+            debug('content-type %j', req.headers['content-type']);
+
+            // determine if request should be parsed
+            if (!shouldParse(req)) {
+                debug('skip parsing');
+                return next();
+            }
+
+            // read
+            read(req, res, next, parse, debug, {
+                encoding: null,
+                inflate: inflate,
+                limit: limit,
+                verify: verify
+            })
+        }
+    }
+
+    Object.defineProperty(bodyParser, 'msgpack', {
+        configurable: true,
+        enumerable: true,
+        get: function() {
+            return extension;
+        }
+    });
+};
